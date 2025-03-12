35baad217a8444d39c4cda4ef2b89d74fcf8dc86
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2025-03-12 10:00:54 +0530

    Colour

--- 5_Colour.cpp
+++ 5_Colour.cpp
@@ -5,10 +5,15 @@ int main() {
     start_color();           // Enable color mode
 
     init_pair(1, COLOR_RED, COLOR_BLACK);   // Define color pair (ID 1)
+    init_pair(2, COLOR_GREEN, COLOR_WHITE);   // Define color pair (ID 2)
     
     attron(COLOR_PAIR(1));   // Activate color pair 1
     printw("This text is RED!\n");
     attroff(COLOR_PAIR(1));  // Deactivate color
+
+    attron(COLOR_PAIR(2));   // Activate color pair 2
+    printw("This text is GREEN!\n");
+    attroff(COLOR_PAIR(2));  // Deactivate color
 
     getch();                 // Wait for key press
     endwin();                // Restore terminal
